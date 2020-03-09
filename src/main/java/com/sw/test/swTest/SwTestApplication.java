48713a95d8a92af03531beba1adeb9ed0a44663b
shaowei <1175233197@qq.com>
2020-03-09 11:19:05 +0800

    Initial commit

---
+++ src/main/java/com/sw/test/swTest/SwTestApplication.java
@@ -0,0 +1,13 @@
+package com.sw.test.swTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sw/test/swTest/SwTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sw.test.swTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
